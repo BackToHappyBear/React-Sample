85bfcfddacf075a67962227397cc0600ed9bfd6c
xingxuan <359499848@qq.com>
2019-06-24 17:09:17 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,59 @@
-import 'react-hot-loader'
+import { hot } from 'react-hot-loader/root'
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+const routes = [
+  {
+    path: '/',
+    exact: true,
+    name: 'home',
+    sidebar: () => <div>home!</div>,
+    component: () => <h2>Home</h2>,
+  },
+  {
+    path: '/bubblegum',
+    name: 'bubblegum',
+    sidebar: () => <div>bubblegum!</div>,
+    component: () => <h2>Bubblegum</h2>,
+  },
+  {
+    path: '/shoelaces',
+    name: 'shoelaces',
+    sidebar: () => <div>shoelaces!</div>,
+    component: () => <h2>Shoelaces</h2>,
+  },
+]
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <div style={{ display: 'flex' }}>
+        <div
+          style={{
+            padding: '10px',
+            width: '40%',
+            height: '100vh',
+            background: '#f0f0f0',
+            flex: 1,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <ul style={{ listStyleType: 'none', padding: 0 }}>
+            {routes.map((route, index) => (
+              <li key={index}>
+                <Link to={route.path}>{route.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div style={{ flex: 7, padding: '10px' }}>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} exact={route.exact} component={route.component} />
+          ))}
+        </div>
+      </div>
+    </Router>
   )
 }
 
-export default App
+export default hot(App)
