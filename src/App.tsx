0ed4f545ba53fc63dcf2e591fca8c0a722fc50a4
xingxuan <359499848@qq.com>
2019-06-24 11:06:34 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'react-hot-loader'
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 const App: React.FC = () => {
   return (
@@ -20,7 +21,7 @@ const App: React.FC = () => {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
