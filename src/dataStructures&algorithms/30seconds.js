3f81f74e463078aa668a80ff7f986a746ee8149a
邢璇 <xingxuan@xingxuandeMacBook-Pro.local>
2020-04-26 23:35:04 +0800

    feat(all): add travis

--- src/dataStructures&algorithms/30seconds.js
+++ src/dataStructures&algorithms/30seconds.js
@@ -28,3 +28,34 @@ const pipeFunctions = (...fns) => (...args) => fns.reduce((p, n) => n(p(...args)
 // TITLE: composeRight  compose(fn1, fn2, fn3)  fn1(fn2(fn3(args)))
 const composeRight1 = (...fns) => fns.reduce((f, g) => (...args) => g(f(...args)))
 const compseRight2 = (...fns) => (...args) => fns.reduceRight((f, g) => g(f(...args)))
+
+// TITLE: dig
+const dig = (obj, target) => {
+  for (const key in obj) {
+    if (key === target) {
+      return obj[key]
+    } else {
+      if (typeof obj[key] === 'object') {
+        return dig(obj[key], target)
+      }
+    }
+  }
+}
+
+const dig2 = (obj, target) =>
+  target in obj
+    ? obj[target]
+    : Object.values(obj).reduce((acc, val) => {
+        if (acc !== undefined) return acc
+        if (typeof val === 'object') return dig2(val, target)
+      })
+
+const data = {
+  level1: {
+    level2: {
+      level3: 'some data',
+    },
+  },
+}
+dig(data, 'level3') // 'some data'
+dig(data, 'level4') // undefined
