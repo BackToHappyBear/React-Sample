3a1e60ee99587f19a487696b184e229007c5c365
邢璇 <xingxuan@xingxuandeMacBook-Pro.local>
2020-05-04 18:16:10 +0800

    WIP:

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -185,8 +185,8 @@ module.exports = function(webpackEnv) {
       globalObject: 'this',
     },
     optimization: {
-      moduleIds: 'named',
-      chunkIds: 'named',
+      moduleIds: 'hashed',
+      // chunkIds: 'named',
       minimize: isEnvProduction,
       minimizer: [
         // This is only used in production mode
