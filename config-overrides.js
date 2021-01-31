bbcf0a30d269d9c572499d342e8b826082854afb
xuan <35949948@qq.com>
2021-01-31 20:09:05 +0800

    chore(wepack): long-term-cache

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const { override, fixBabelImports, setWebpackOptimizationSplitChunks } = require('customize-cra')
 
 const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
@@ -25,19 +26,19 @@ module.exports = override(
         name: 'chunk-libs',
         test: /[\\/]node_modules[\\/]/,
         priority: 10,
-        chunks: 'initial', // 只打包初始时依赖的第三方
+        chunks: 'initial',
       },
-      elementUI: {
-        name: 'chunk-antd', // 单独将 elementUI 拆包
+      antd: {
+        name: 'chunk-antd',
         priority: 20, // 权重要大于 libs 和 app 不然会被打包进 libs 或者 app
         test: /[\\/]node_modules[\\/]antd[\\/]/,
       },
       commons: {
         name: 'chunk-commons',
-        test: /[\\/]src[\\/]components[\\/]/, // 可自定义拓展你的规则
-        minChunks: 1, // 最小共用次数
+        test: path.join(__dirname, 'src/components'), // 可自定义拓展你的规则
+        minChunks: 1,
         priority: 5,
-        reuseExistingChunk: true,
+        // reuseExistingChunk: true,
       },
     },
   }),
